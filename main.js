0b38590cff7677132cec4074c6439685acf81844
turtleprince <81336142+turtleprince@users.noreply.github.com>
2022-05-12 18:00:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,6 +12,20 @@ function my_model_loaded(){
 
 }
 
-function got_result(error, result){
-console.log("got_result");
-}
+function got_result(error, result)
+    console.log("got_result");
+    if (error){
+        console.error(error);
+    }
+    else {
+        console.log(result);
+       document.getElementById("result_label").innerHTML = "I can hear " + result[0].label;
+       document.getElementById("result_confidence").innerHTML = "Accuracy is " + (result[0].confidence*100).toFixed(2)  + "%";
+
+       random_red = Math.floor(Math.random()*255) + 1 ;
+       random_green = Math.floor(Math.random()*255) + 1 ;
+       random_blue = Math.floor(Math.random()*255) + 1 ;
+
+       document.getElementById("result_label").style.color = "rgb(" + random_red + "," + random_green + "," + random_blue+")";
+       document.getElementById("result_confidence").style.color =  "rgb(" +random_red + "," + random_green + "," + random_blue+")";
+    }
